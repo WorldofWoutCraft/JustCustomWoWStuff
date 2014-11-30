b59bf80b561bfac2d0d213f7f1208606e960703d
woutwoot <woutwoot13@gmail.com>
2014-11-30 01:24:53 +0100

    Add /wow head name

---
+++ src/main/java/com/woutwoot/jcwows/commands/WoW_head.java
@@ -0,0 +1,32 @@
+package com.woutwoot.jcwows.commands;
+
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.SkullMeta;
+
+/**
+ * Created by Wout on 30/11/2014.
+ */
+public class WoW_head extends WoW_Command {
+
+    @Override
+    public void process(CommandSender sender, String[] args) {
+        Player p = null;
+        if (sender instanceof Player) {
+            p = (Player) sender;
+            ItemStack skull = new ItemStack(397, 1, (short) 3);
+            SkullMeta meta = (SkullMeta) skull.getItemMeta();
+            meta.setOwner(args[0]);
+            skull.setItemMeta(meta);
+            p.getInventory().addItem(skull);
+        } else {
+            sender.sendMessage("You have to be a player!");
+        }
+    }
+
+    @Override
+    public void sendHelp(CommandSender sender) {
+
+    }
+}
