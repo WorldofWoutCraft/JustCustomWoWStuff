2231a76b8d3ae3b16e139fb045290fc44a9be94c
woutwoot <woutwoot13@gmail.com>
2014-12-05 12:31:27 +0100

    /wow crash Mohahahahahahaa

--- src/main/java/com/woutwoot/jcwows/commands/WoW_crash.java
+++ src/main/java/com/woutwoot/jcwows/commands/WoW_crash.java
@@ -1,14 +1,10 @@
 package com.woutwoot.jcwows.commands;
 
-import com.woutwoot.jcwows.Main;
-import com.woutwoot.jcwows.tools.UUIDFetcher;
-import net.minecraft.server.v1_8_R1.EntityPlayer;
-import net.minecraft.server.v1_8_R1.PacketPlayOutSpawnEntityLiving;
+import org.bukkit.Effect;
 import org.bukkit.command.CommandSender;
-import org.bukkit.craftbukkit.v1_8_R1.entity.CraftPlayer;
 import org.bukkit.entity.Player;
 
-import java.util.UUID;
+import java.util.Random;
 
 /**
  * Created by Wout on 30/11/2014.
@@ -17,15 +13,11 @@ public class WoW_crash extends WoW_Command {
 
     @Override
     public void process(CommandSender sender, String[] args) {
-        if (args.length > 1 && args[0] != null && !args[0].equals("")) {
-            try {
-                UUID uuid = UUIDFetcher.getUUIDOf(args[0]);
-                Player player = Main.getInstance().getServer().getPlayer(uuid);
-                EntityPlayer nmsPlayer = ((CraftPlayer) player).getHandle();
-                nmsPlayer.playerConnection.sendPacket(new PacketPlayOutSpawnEntityLiving(nmsPlayer));
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
+        if (args.length > 0 && args[0] != null && !args[0].equals("")) {
+            Player p = (Player) sender;
+            Random r = new Random();
+            for (int i = 1; i < 10000; i++)
+                p.playEffect(p.getLocation().add(r.nextInt(20) - 10, r.nextInt(10) - 5, r.nextInt(10) - 5), Effect.MOBSPAWNER_FLAMES, 0);
         }
     }
 
